29d63760628d16549fbc6cc092b8707897285dd4
Fayek Helmi <fayek.helmi@gmail.com>
2017-02-18 23:45:36 -0500

    completed the front page of the website along with the media queries for responsiveness - minor bugs remain for iOS devices, the navbar highlight when clicked orhover does not fill full navbar height - cannot figure out why - browser-sync hosted html does not register the meta viewport html tag but when hosted online it works - missing pages will be blog - custom projects pages (could be either a basic search page with a filter or custom pages with a wp query) haven't decided yet - when site will completed it will be backed to a branch and then a new brach will be created for converting it to php for wordpress

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,31 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();;
+var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 var sass        = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // browser-sync task for starting the server.
 
 
 // Sass task, will run when any SCSS files change & BrowserSync
 // will auto-update browsers
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
 gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
         .pipe(sass())
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./'))
         .pipe(reload({stream:true}));
 });
 
 // Default task to be run with `gulp`
-gulp.task('default', ['sass'], function () {
+gulp.task('default', ['sass', 'browser-sync'], function () {
     gulp.watch("sass/**/*.scss", ['sass']);
 
 });
