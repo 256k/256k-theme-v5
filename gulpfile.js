07d041fec70cb1787d413a1d67597b21717e89fd
Fayek Helmi <fayek.helmi@gmail.com>
2017-03-04 22:07:08 -0500

    begun the wp conversion following the wordpress theme build playlist on youtube - will start with empty php files and slowly import html content from template files - stylesheet is also integrated

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,48 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create();;
 var reload      = browserSync.reload;
 var sass        = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 
 // browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+    //watch files
+    var files = [
+    './style.css',
+    './*.php',
+    './js/*.js'
+    ];
 
+    //initialize browsersync
+    browserSync.init(files, {
+    //browsersync with a php server
+    proxy: {
+        target: "localhost:8080/wordpress",
+        proxyRes: [
+            function (res) {
+                res.headers["Expires"] = "0";
+                res.headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+                res.headers["Pragma"] = "no-cache";
+            }
+        ]
+    }
+
+
+    });
+
+});
 
 // Sass task, will run when any SCSS files change & BrowserSync
 // will auto-update browsers
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-});
 gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
         .pipe(sass())
-        .pipe(autoprefixer())
         .pipe(gulp.dest('./'))
         .pipe(reload({stream:true}));
 });
 
 // Default task to be run with `gulp`
 gulp.task('default', ['sass', 'browser-sync'], function () {
-  gulp.watch("*.html").on("change", reload);
     gulp.watch("sass/**/*.scss", ['sass']);
+    gulp.watch('./*.php').on('change', browserSync.reload);
 
 });
